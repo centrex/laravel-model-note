238c82303e2a9827a103b00bbe6c0f3995284839
rochi88 <raisul.me@gmail.com>
2024-06-02 15:13:41 +0600

    fix migration

--- src/LaravelModelNoteServiceProvider.php
+++ src/LaravelModelNoteServiceProvider.php
@@ -19,7 +19,7 @@ class LaravelModelNoteServiceProvider extends ServiceProvider
          */
         // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'laravel-model-note');
         // $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-model-note');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         if ($this->app->runningInConsole()) {
